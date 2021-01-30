27a1a3294cf9ee824de359e0fc20ba6d0f1ccef7
Alejandro Fraga Cimadevila <alex_fraga95@hotmail.com>
2021-01-30 20:49:03 +0100

    GameObjectHelper created

---
+++ 2051/Assets/Scripts/GameObjectHelper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public static class GameObjectHelper
+{
+    public static void SetVisible(Image image, bool visible = true)
+    {
+        Color newColor = image.color;
+        newColor.a = visible ? 1 : 0;
+        image.color = newColor;
+    }
+}
